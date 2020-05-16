ec802bc6fbaaf7fab98f89778369913c1d16e8a1
philipjones <phil.jones.24.4@gmail.com>
2020-05-16 20:33:53 +0100

    WIP - Initial

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -7,3 +8,8 @@ def index():
     user = {'username': 'Phil'}
     version = "1.0.0.1"
     return render_template('index.html', title='Home', user=user, version=version)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
